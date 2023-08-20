287786f3f1e61f204700812176b8ed261c8e1e57
Stefano <stdemicheli@protonmail.com>
2023-08-20 18:28:24 +0700

    Adds byte opcodes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,5 @@
 use primitive_types::U256;
-use std::{fmt::Write, str::FromStr, clone};
+use std::{fmt::Write};
 
 struct MemoryError;
 
@@ -8,7 +8,7 @@ struct Memory {
 }
 
 impl Memory {
-    fn store(&mut self, offset: usize, value: U256) -> Result<bool, MemoryError> {
+    fn store(mut self, offset: usize, value: U256) -> Result<bool, MemoryError> {
         // let max = U256::max_value();
 
         if offset < 0 { //|| offset > max
@@ -19,13 +19,12 @@ impl Memory {
             return Err(MemoryError);
         }
 
-        // self.memory[offset] = value;
-        self.memory.push(value);
+        self.memory[offset] = value;
 
         Ok(true)
     }
 
-    fn load(&self, offset: usize) -> Result<U256, MemoryError> {
+    fn load(self, offset: usize) -> Result<U256, MemoryError> {
         if offset < 0 {
             return Err(MemoryError);
         }
@@ -43,29 +42,22 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
     // TODO: Implement me
     let mut stack: Vec<U256> = Vec::new();
 
-    let mut opcode: String = String::new();
+    let mut opcode: u8 = 0;
     
     let mut pc: usize = 0;
 
-    let mut memory = Memory{ memory: vec![], };
+    let mut memory = &Memory{ memory: vec![], };
 
-    // for (i, data) in code.as_ref().iter().enumerate() {
     while pc < code.as_ref().len() {
-        // if i % 2 == 0 {
-
-            opcode = String::new();
-            
-            // write!(&mut opcode, "{:X}", data).expect("Unable to write");
-            write!(&mut opcode, "{:X}", code.as_ref()[pc]).expect("Unable to write");
-
-            // println!("adding opcode {}", opcode);
+            opcode = code.as_ref()[pc];
 
             println!("starting operation {}", opcode);
-            match opcode.trim() {
-                "00" => {
+            match opcode {
+                STOP => {
                     pc += 1;
+                    break;
                 },
-                "1" => {
+                ADD => {
                     let a = stack.pop();
                     let b = stack.pop();
                     if let (Some(a), Some(b)) = (a, b) {
@@ -74,10 +66,10 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "50" => {
+                POP => {
                     stack.pop();
                 },
-                "2" => {
+                MUL => {
                     let a = stack.pop();
                     let b = stack.pop();
                     if let (Some(a), Some(b)) = (a, b) {
@@ -86,7 +78,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "3" => {
+                SUB => {
                     let a = stack.pop();
                     let b = stack.pop();
                     if let (Some(a), Some(b)) = (a, b) {
@@ -95,7 +87,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "4" | "5" => {
+                DIV | SDIV => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -111,7 +103,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "6" | "7" => {
+                MOD | SMOD => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -127,7 +119,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "10" => {
+                LT => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -139,7 +131,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "12" => {
+                SLT => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -151,7 +143,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "11" | "13" => {
+                GT | SGT => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -161,7 +153,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "14" => {
+                EQ => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -173,7 +165,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "15" => {
+                ISZERO => {
                     let a = stack.pop();
                     
                     if let Some(a) = a {
@@ -182,7 +174,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "16" => {
+                AND => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -194,7 +186,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "17" => {
+                OR => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -206,7 +198,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "18" => {
+                XOR => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -218,7 +210,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "19" => {
+                NOT => {
                     let a = stack.pop();
                     
                     if let Some(a) = a {
@@ -228,7 +220,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "1A" => {
+                BYTE => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -242,46 +234,37 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "7F" => { // push32
-                    let data = &code.as_ref()[pc + 1 .. pc + 33];
+                PUSH32 => {
+                    let data = &code.as_ref()[pc + 1 .. pc + 32];
                     let value = U256::from(data);
                     stack.push(value);
                     pc += 32;
                 },
-                "51" => { // mload
-                    let offset = stack.pop();
-                    if let Some(offset) = offset {
-                        let loaded_value = &memory.load(offset.as_usize());
+                // "51" => { // mload
+                //     let offset = stack.pop();
+                //     if let Some(offset) = offset {
+                //         let value = memory.load(offset.as_usize());
 
-                        let value = match loaded_value {
-                            Ok(value) => value,
-                            Err(error) => panic!("Problem loading"),
-                        };
-                        println!("value: {}", value);
-                        stack.push(*value);
-                    }
-                },
-                "52" => { // mstore
-                    let offset = stack.pop();
-                    let value = stack.pop();
-                    if let (Some(offset), Some(value)) = (offset, value) {
-                        println!("offset: {}", offset);
-                        println!("value: {}", value);
-                        let result = &memory.store(offset.as_usize(), value);
-                        let success = match result {
-                            Ok(success) => success,
-                            Err(error) => panic!("Failed to mstore"),
-                        };
-                        println!("{success}");
-                    }
-                },
-                "56" => {
+                //         match value {sdfds
+                //             Ok(value) => stack.push(value);
+                //             Err(err) => panic!("Load error: {:?}", error);
+                //         };
+                //     }
+                // },
+                // "52" => { // mstore
+                //     let offset = stack.pop();
+                //     let value = stack.pop();
+                //     if let (Some(offset), Some(value)) = (offset, value) {
+                //         memory.store(offset.as_usize(), value);
+                //     }
+                // },
+                JUMP => {
                     let destination = stack.pop();
                     if let Some(destination) = destination {
                         pc = destination.as_usize();
                     }
                 },
-                "57" => {
+                JUMPI => {
                     let destination = stack.pop();
                     let condition = stack.pop();
                     if let (Some(destination), Some(condition)) = (destination, condition)  {
@@ -290,7 +273,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         }
                     }
                 },
-                "58" => {
+                PC => {
                     let pc = pc.clone();
                     stack.push(U256::from(pc));
                 },
@@ -301,7 +284,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                 //     //     pc = destination.as_usize();
                 //     // }
                 // },
-                "60" => {
+                PUSH1 => {
                     let next_value = code.as_ref().get(pc + 1);
                     if let Some(value) = next_value {
                         let value = U256::from_big_endian(&[*value]);
@@ -310,13 +293,13 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "61" => {
+                PUSH2 => {
                     let data = &code.as_ref()[pc + 1 .. pc + 2];
                     let value = U256::from(data);
                     stack.push(value);
                     pc += 2;
                 },
-                "80" => {
+                DUP1 => {
                     let latest = stack.pop();
                     if let Some(value) = latest {
                         let dup = value.clone();
@@ -324,7 +307,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "81" => {
+                DUP2 => {
                     let second_last = stack.get(stack.len() - 2);
                     if let Some(value) = second_last {
                         let dup = value.clone();
@@ -332,7 +315,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "82" => {
+                DUP3 => {
                     let second_last = stack.get(stack.len() - 3);
                     if let Some(value) = second_last {
                         let dup = value.clone();
@@ -340,7 +323,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "90" => {
+                SWAP1 => {
                     let a = stack.pop();
                     let b = stack.pop();
                     
@@ -350,7 +333,7 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                         pc += 1;
                     }
                 },
-                "92" => {
+                SWAP3 => {
                     let b = stack.remove(3);
                     let a = stack.remove(0);
                     // let b = stack.get(2);
@@ -377,75 +360,160 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
     return stack.into_iter().rev().collect();
 }
 
-fn getBigInt(opcode: &str, value: &u8) -> Option<U256> {
-    println!("opcode {}", opcode);
-    match opcode.trim() {
-        "00" => None,
-        "60" => {
-            println!("hello");
-            let value = U256::from_big_endian(&[*value]);
-            let bigint = value; // this panics when it can't unwrap
-            // This is safer:
-            // let bigint = match string {
-            //     Ok(bigint) =>  bigint,
-            //     Err(error) => panic!("Problem conversion: {:?}", error),
-            // };
-            Some(bigint)
-        },
-        _ => None
-    }
-}
+// revm
+pub struct OpCode(u8);
 
-struct TryFromSliceError(());
+pub const STOP: u8 = 0x00;
+pub const ADD: u8 = 0x01;
+pub const MUL: u8 = 0x02;
+pub const SUB: u8 = 0x03;
+pub const DIV: u8 = 0x04;
+pub const SDIV: u8 = 0x05;
+pub const MOD: u8 = 0x06;
+pub const SMOD: u8 = 0x07;
+pub const ADDMOD: u8 = 0x08;
+pub const MULMOD: u8 = 0x09;
+pub const EXP: u8 = 0x0a;
+pub const SIGNEXTEND: u8 = 0x0b;
 
-fn slice_to_array_32<T>(slice: &[T]) -> Result<&[T; 32], TryFromSliceError> {
-    if slice.len() == 32 {
-        let ptr = slice.as_ptr() as *const [T; 32];
-        unsafe {Ok(&*ptr)}
-    } else {
-        Err(TryFromSliceError(()))
-    }
-}
+pub const LT: u8 = 0x10;
+pub const GT: u8 = 0x11;
+pub const SLT: u8 = 0x12;
+pub const SGT: u8 = 0x13;
+pub const EQ: u8 = 0x14;
+pub const ISZERO: u8 = 0x15;
+pub const AND: u8 = 0x16;
+pub const OR: u8 = 0x17;
+pub const XOR: u8 = 0x18;
+pub const NOT: u8 = 0x19;
+pub const BYTE: u8 = 0x1a;
 
-fn something() {
-    let string = String::from("hello");
-    take_ownership(string);
+pub const CALLDATALOAD: u8 = 0x35;
+pub const CALLDATASIZE: u8 = 0x36;
+pub const CALLDATACOPY: u8 = 0x37;
+pub const CODESIZE: u8 = 0x38;
+pub const CODECOPY: u8 = 0x39;
 
-    let x = 5;
-    make_copy(x);
-    println!("{}", x);
+pub const SHL: u8 = 0x1b;
+pub const SHR: u8 = 0x1c;
+pub const SAR: u8 = 0x1d;
+pub const KECCAK256: u8 = 0x20;
+pub const POP: u8 = 0x50;
+pub const MLOAD: u8 = 0x51;
+pub const MSTORE: u8 = 0x52;
+pub const MSTORE8: u8 = 0x53;
+pub const JUMP: u8 = 0x56;
+pub const JUMPI: u8 = 0x57;
+pub const PC: u8 = 0x58;
+pub const MSIZE: u8 = 0x59;
+pub const JUMPDEST: u8 = 0x5b;
 
-    let string1 = String::from("hello");
-    let returned_string = return_ownership(string1);
-    // println!("{}", string1); // error, string1 moves to return_ownership
-    println!("{}", returned_string); // ok
-}
+pub const TLOAD: u8 = 0x5c;
+pub const TSTORE: u8 = 0x5d;
 
-fn take_ownership(string: String) {
-    println!("{}", string);
-}
+pub const MCOPY: u8 = 0x5e;
+pub const PUSH0: u8 = 0x5f;
+pub const PUSH1: u8 = 0x60;
+pub const PUSH2: u8 = 0x61;
+pub const PUSH3: u8 = 0x62;
+pub const PUSH4: u8 = 0x63;
+pub const PUSH5: u8 = 0x64;
+pub const PUSH6: u8 = 0x65;
+pub const PUSH7: u8 = 0x66;
+pub const PUSH8: u8 = 0x67;
+pub const PUSH9: u8 = 0x68;
+pub const PUSH10: u8 = 0x69;
+pub const PUSH11: u8 = 0x6a;
+pub const PUSH12: u8 = 0x6b;
+pub const PUSH13: u8 = 0x6c;
+pub const PUSH14: u8 = 0x6d;
+pub const PUSH15: u8 = 0x6e;
+pub const PUSH16: u8 = 0x6f;
+pub const PUSH17: u8 = 0x70;
+pub const PUSH18: u8 = 0x71;
+pub const PUSH19: u8 = 0x72;
+pub const PUSH20: u8 = 0x73;
+pub const PUSH21: u8 = 0x74;
+pub const PUSH22: u8 = 0x75;
+pub const PUSH23: u8 = 0x76;
+pub const PUSH24: u8 = 0x77;
+pub const PUSH25: u8 = 0x78;
+pub const PUSH26: u8 = 0x79;
+pub const PUSH27: u8 = 0x7a;
+pub const PUSH28: u8 = 0x7b;
+pub const PUSH29: u8 = 0x7c;
+pub const PUSH30: u8 = 0x7d;
+pub const PUSH31: u8 = 0x7e;
+pub const PUSH32: u8 = 0x7f;
+pub const DUP1: u8 = 0x80;
+pub const DUP2: u8 = 0x81;
+pub const DUP3: u8 = 0x82;
+pub const DUP4: u8 = 0x83;
+pub const DUP5: u8 = 0x84;
+pub const DUP6: u8 = 0x85;
+pub const DUP7: u8 = 0x86;
+pub const DUP8: u8 = 0x87;
+pub const DUP9: u8 = 0x88;
+pub const DUP10: u8 = 0x89;
+pub const DUP11: u8 = 0x8a;
+pub const DUP12: u8 = 0x8b;
+pub const DUP13: u8 = 0x8c;
+pub const DUP14: u8 = 0x8d;
+pub const DUP15: u8 = 0x8e;
+pub const DUP16: u8 = 0x8f;
+pub const SWAP1: u8 = 0x90;
+pub const SWAP2: u8 = 0x91;
+pub const SWAP3: u8 = 0x92;
+pub const SWAP4: u8 = 0x93;
+pub const SWAP5: u8 = 0x94;
+pub const SWAP6: u8 = 0x95;
+pub const SWAP7: u8 = 0x96;
+pub const SWAP8: u8 = 0x97;
+pub const SWAP9: u8 = 0x98;
+pub const SWAP10: u8 = 0x99;
+pub const SWAP11: u8 = 0x9a;
+pub const SWAP12: u8 = 0x9b;
+pub const SWAP13: u8 = 0x9c;
+pub const SWAP14: u8 = 0x9d;
+pub const SWAP15: u8 = 0x9e;
+pub const SWAP16: u8 = 0x9f;
+pub const RETURN: u8 = 0xf3;
+pub const REVERT: u8 = 0xfd;
+pub const INVALID: u8 = 0xfe;
+pub const ADDRESS: u8 = 0x30;
+pub const BALANCE: u8 = 0x31;
+pub const BASEFEE: u8 = 0x48;
+pub const ORIGIN: u8 = 0x32;
+pub const CALLER: u8 = 0x33;
+pub const CALLVALUE: u8 = 0x34;
+pub const GASPRICE: u8 = 0x3a;
+pub const EXTCODESIZE: u8 = 0x3b;
+pub const EXTCODECOPY: u8 = 0x3c;
+pub const EXTCODEHASH: u8 = 0x3f;
+pub const RETURNDATASIZE: u8 = 0x3d;
+pub const RETURNDATACOPY: u8 = 0x3e;
+pub const BLOCKHASH: u8 = 0x40;
+pub const COINBASE: u8 = 0x41;
+pub const TIMESTAMP: u8 = 0x42;
+pub const NUMBER: u8 = 0x43;
+pub const DIFFICULTY: u8 = 0x44;
+pub const GASLIMIT: u8 = 0x45;
+pub const SELFBALANCE: u8 = 0x47;
+pub const SLOAD: u8 = 0x54;
+pub const SSTORE: u8 = 0x55;
+pub const GAS: u8 = 0x5a;
 
-fn make_copy(int: i32) {
-    println!("{}", int);
-}
+pub const LOG0: u8 = 0xa0;
+pub const LOG1: u8 = 0xa1;
+pub const LOG2: u8 = 0xa2;
+pub const LOG3: u8 = 0xa3;
+pub const LOG4: u8 = 0xa4;
 
-fn take_ownership2(ints: Vec<i32>) {
-    for int in ints {
-        println!("{}", int);
-    }
-}
-
-fn return_ownership(string: String) -> String {
-    string
-}
-
-
-fn hello() {
-    let hi = String::from("fsdfsds");
-    let length = take_string(&hi);
-
-}
-
-fn take_string(string: &String) -> usize {
-    return string.len();
-}
+pub const CREATE: u8 = 0xf0;
+pub const CREATE2: u8 = 0xf5;
+pub const CALL: u8 = 0xf1;
+pub const CALLCODE: u8 = 0xf2;
+pub const DELEGATECALL: u8 = 0xf4;
+pub const STATICCALL: u8 = 0xfa;
+pub const SELFDESTRUCT: u8 = 0xff;
+pub const CHAINID: u8 = 0x46;
